50a2a9cb929ef29ad6958c8c93380ac8cb81fd49
Karan Gulati <karan.gulati1828@gmail.com>
2026-04-19 23:31:01 +0530

    updated features

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-function Footer(){
-    const currentYear= new Date().getFullYear();
-return (
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
     <footer>
-    <p>Copyright © {currentYear} </p>
-    </footer> 
-    );
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
+
 export default Footer;
